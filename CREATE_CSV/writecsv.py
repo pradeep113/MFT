34ee2c5ffdfd13567a2955dbe7a6fbd536f0a328
pradeep113 <37507656+pradeep113@users.noreply.github.com>
2025-04-07 18:09:17 +0530

    write to csv file using streamlit

---
+++ CREATE_CSV/writecsv.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import os
+
+# Title of the app
+st.title("MFT Developer Tool")
+
+pattern = st.selectbox("SELECT TRANSFER PATTERN", ["SFTP_PULL_TO_INT_SHARE_TEMPLATE", "SFTP_PULL_TO_INT_SHAREPOINT_TEMPLATE", "SFTP_PULL_TO_INT_BLOB_TEMPLATE", "SHARE_PULL_TO_INT_SHAREPOINT_TEMPLATE"])
+
+
+# Create text input fields for six values
+value1 = st.text_input("INTERFACE")
+value2 = st.text_input("SOURCE SYSTEM")
+value3 = st.text_input("TARGET_SYSTEM")
+value4 = st.text_input("SOURCE SERVER")
+value5 = st.text_input("SOURCE PATH")
+value6 = st.text_input("TARGET SERVER")
+value7 = st.text_input("TARGET PATH")
+
+# Define the CSV file path
+csv_file_path = "output.csv"
+
+# Button to submit the inputs
+if st.button("Submit"):
+    # Check if all inputs are provided
+    if all([value1, value2, value3, value4, value5]):
+        # Create a DataFrame to write to CSV
+        df = pd.DataFrame([
+	    ["PATTERN_VALUE", pattern],
+            ["INTERFACE_VALUE", value1],
+	    ["SOURCESYSTEM_VALUE", value2],
+            ["TARGETSYSTEM_VALUE", value3],
+            ["SOURCESERVER_VALUE", value4],
+            ["SOURCEPATH_VALUE", value5],
+            ["TARGETSERVER_VALUE", value6],
+            ["TARGETPATH_VALUE", value7]
+        ], columns=["Variable", "Value"])
+
+        # Check if the CSV file already exists
+        if os.path.isfile(csv_file_path):
+            # Append to the existing CSV file
+            df.to_csv(csv_file_path, index=False)
+
+        else:
+            # Create a new CSV file
+            df.to_csv(csv_file_path, index=False)
+
+        st.success("Values saved to CSV!")
+    else:
+        st.error("Please fill in all fields before submitting.")
